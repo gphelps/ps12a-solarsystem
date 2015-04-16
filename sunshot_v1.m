119a45c9dfdbd84b14e4dc3fddc7c52572f3bdcb
sohumpawar <sohum@pawarpower.com>
2015-04-16 14:15:18 -0400

    Create sunshot_v1.m

---
+++ sunshot_v1.m
@@ -0,0 +1,17 @@
+% Workspace
+clear
+close all
+
+% Variables!
+G = 6.67E-11;
+m_sun = 1.99E30;
+m_earth = 5.98E24;
+omega_0 =
+x_0 = 
+y_0 = 
+
+% % Steps
+% Define starting omega, x,y
+    % Could potentially use mean values for this
+% Separate Force into x and y components
+% Find v_x(t), x(t); v_y(t), y(t)
