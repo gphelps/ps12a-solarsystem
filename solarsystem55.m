88245a6bf799515be77c789d8d72e752bb89b14e
Sohum Pawar <sohum@pawarpower.com>
2015-05-05 23:26:13 -0400

    "Checked if m/day -> m/s was the issue... no luck"

--- solarsystem55.m
+++ solarsystem55.m
@@ -14,8 +14,8 @@ m_mercury = 3.302E23;                                   % kg
 x0_mercury = (-3.589070923093208E-01)*AU;               % m
 y0_mercury = (8.140221586479408E-02)*AU;                % m
 r0_mercury = sqrt((x0_mercury^2)+(y0_mercury^2));       % m
-vx0_mercury = (-1.205489207680449E-02)*AU;              % m/day
-vy0_mercury = (-2.623106213623277E-02)*AU;              % m/day
+vx0_mercury = ((-1.205489207680449E-02)*AU)/86400;              % m/day
+vy0_mercury = ((-2.623106213623277E-02)*AU)/86400;              % m/day
 
 % Initialize
 x_mercury = [];
